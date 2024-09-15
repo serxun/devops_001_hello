fdfc998a51aae3f2025d9b18468c6b9407b1d547
Emre Kanlıbaş <74323128+serxun@users.noreply.github.com>
2024-09-15 15:34:14 -0700

    test1

---
+++ src/main/java/com/example/devops_001_hello1/Devops001Hello1Application.java
@@ -0,0 +1,13 @@
+package com.example.devops_001_hello1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops001Hello1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops001Hello1Application.class, args);
+    }
+
+}
