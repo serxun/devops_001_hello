f24bf5205274e6315f7f9bfe10fc309a2dbb994d
Emre Kanlıbaş <74323128+serxun@users.noreply.github.com>
2024-09-24 20:12:14 -0700

    Devops Hello V1

--- src/main/java/com/example/devops_001_hello1/controller/Devopscontroller.java
+++ src/main/java/com/example/devops_001_hello1/controller/Devopscontroller.java
@@ -1,4 +1,32 @@
 package com.example.devops_001_hello1.controller;
 
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+// http://localhost:8080/
+
+
+@RestController
+@RequestMapping
 public class Devopscontroller {
+
+    private final TaskExecutionProperties taskExecutionProperties;
+
+    public Devopscontroller(TaskExecutionProperties taskExecutionProperties) {
+        this.taskExecutionProperties = taskExecutionProperties;
+    }
+
+    @GetMapping
+    public String devopsHello() {
+        return "Devops Hello :" + LocalDate.now();
+
+
+    }
+
+
 }
